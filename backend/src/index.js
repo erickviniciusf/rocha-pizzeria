69967145b3daeebaa42cff11025dede63405ee1a
Erick Vinicius <erick.viniciusfff@gmail.com>
2026-05-14 01:04:48 -0300

    feat: server setup and database connection

---
+++ backend/src/index.js
@@ -0,0 +1,27 @@
+import 'dotenv/config'; 
+import express from 'express'; 
+import cors from 'cors'; 
+import pool from './database.js'; 
+
+const app = express();
+const PORT = process.env.PORT || 3000
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.json({'Mensagem': 'API funcionando normalmente'})
+})
+
+async function conexaoComBD(){
+    const resultado = await pool.query('SELECT 1')
+    console.log("Banco conectado com sucesso")
+} 
+
+conexaoComBD().catch(err => {
+    console.log('Erro ao conectar ao banco', err)
+})
+
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`)
+})
